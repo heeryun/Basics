c99c34f97ffd2ca1d17b048f0c90e8b619afb986
Heeryun Kim <basilpigeonkim@gmail.com>
2026-06-25 17:38:18 -0400

    Update and rename guess_age.py to guess_number.py

---
+++ guess_number.py
@@ -0,0 +1,6 @@
+# Here's your first challenge!
+# Make a game that generates a random number from 0 - 10
+# Make the user guess the number, and give a response depending on whether they are right or wrong
+
+# Challenge 1: Make the program say higher or lower based on the guess accurately
+# Challenge 2: Limit the number of guesses that the player gets
