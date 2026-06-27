67cf25345d8ee30b5055399cc0eaacfdb754fee5
Silverwish Waterheart <Rascrivens@github.com>
2026-06-27 14:43:16 -0400

    SIKE! that's the wrong number!

--- guess_number.py
+++ guess_number.py
@@ -7,4 +7,31 @@ import random
 # Challenge 1: Make the program say higher or lower based on the guess accurately
 # Challenge 2: Limit the number of guesses that the player gets
 
-number = random.randint(1,10)
+# number = random.randint(1,10)
+
+number = random.randint(1, 10)
+print("I'm thinking of a number between 0 and 10. What it is?")
+# user_number = input("Enter number: ")
+# number_of_guesses = 0
+
+while True:
+    user_number = input("Enter number: ")
+    try:
+        user_number = int(user_number)
+    except ValueError:
+        print("You're just trying to break my code, aren't you?")
+        continue
+    if int(user_number) > 10: 
+        print("I said 0 - 10, am I muted?")
+        continue
+    if int(user_number) < 0: 
+        print("Thats...not fair.")
+        continue
+    elif int(user_number) > number:
+        print("Where is your head, in the clouds? You're too high!")
+    elif int(user_number) < number:
+        print("You're as short as me! guess higher...")
+    elif int(user_number) == number:
+        print("10 years later...You got it!")
+        break
+    print("guess again")
