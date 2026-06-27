fd6b600849f95b51d3b05621f0f46c421ddbea64
Heeryun <basilpigeonkim@gmail.com>
2026-06-27 13:26:10 -0400

    adding hints for rbk

---
+++ HK_sandbox.py
@@ -0,0 +1,10 @@
+name = "Rebekah"
+
+#print('Hello', name,', nice to meet you', name)
+
+#print(f'Hello {name}, nice to meet you {name}')
+
+numbers = range(0,11)
+
+for i in numbers:
+    print(i)
--- guess_number.py
+++ guess_number.py
@@ -1,6 +1,10 @@
+import random
+
 # Here's your first challenge!
 # Make a game that generates a random number from 0 - 10
 # Make the user guess the number, and give a response depending on whether they are right or wrong
 
 # Challenge 1: Make the program say higher or lower based on the guess accurately
 # Challenge 2: Limit the number of guesses that the player gets
+
+number = random.randint(1,10)
